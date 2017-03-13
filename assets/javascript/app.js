2de17c6baa653270e7e707228009c7f91c7f9f44
Brandon Campbell <Brandon Campbell>
2017-03-13 17:14:51 -0500

    fourth commit

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,10 +1,5 @@
 $('#start').on('click',function() {
-	for (var i=0; i<questions.length; i++) {
-		$('#subwrapper').append('<h2>'+questions[i].question+'</h2>');
-		for(var j=0; j<questions[i].answers.length; j++) {
-			$('#subwrapper').append("<input type='radio' name='question-"+i+"' value=' "+questions[i].answers[j]+"'>"+questions[i].answers[j]);
-		}
-	};
+ 	game.start();
 });
 
 var questions = [{
@@ -20,3 +15,29 @@ var questions = [{
 	answers:["Jerry Only", "Flea", "Rob Trujillo", "Jaco Pastorius"],
 	correctAnswer:"Jerry Only"
 }];
+
+var game = {
+	correct: 0,
+	incorrect: 0,
+	counter: 120,
+	countdown: function(){
+		game.counter--;
+		$('#counter').html(game.counter);
+		if(game.counter<=0){
+			console.log("Time's up.");
+			game.done();
+		}
+	},
+	start: function(){
+		timer = setInterval(game.countdown,1000);
+		$('#subwrapper').prepend('<h2>Time Left: <span id="counter">120</span> Seconds</h2>');
+		$('#start').remove();
+		for (var i=0; i<questions.length; i++) {
+			$('#subwrapper').append('<h2>'+questions[i].question+'</h2>');
+			for(var j=0; j<questions[i].answers.length; j++) {
+				$('#subwrapper').append("<input type='radio' name='question-"+i+"' value=' "+questions[i].answers[j]+"'>"+questions[i].answers[j]);
+			}
+		}
+
+	}
+}
